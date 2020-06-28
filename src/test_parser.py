9f28f3812325a50e9a010e7252c64d833d85f879
mxheller <max.a.heller@gmail.com>
2020-06-27 20:22:34 -0400

    fix tests

--- src/test_parser.py
+++ src/test_parser.py
@@ -4,11 +4,11 @@ from .parser import Parser
 
 class TestParserInit(unittest.TestCase):
     def test_init(self):
-        self.assertEqual(Parser("- abc").content, ["abc"])
-        self.assertEqual(Parser("[abc, sdf]").content, ["abc", "sdf"])
-        self.assertEqual(Parser("cat: dog").content, {"cat": "dog"})
-        self.assertEqual(
-            Parser("cat: [dog,goose]").content, {"cat": ["dog", "goose"]})
+        self.assertEqual(Parser.parse("- abc"), ["abc"])
+        self.assertEqual(Parser.parse("[abc, sdf]"), ["abc", "sdf"])
+        self.assertEqual(Parser.parse("cat: dog"), {"cat": "dog"})
+        self.assertEqual(Parser.parse("cat: [dog,goose]"),
+                         {"cat": ["dog", "goose"]})
 
 
 if __name__ == "__main__":
